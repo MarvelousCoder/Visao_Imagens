a7e6f362a0b4b172cc44a05d8b44aedbbb347f05
Angelic Coder <Raphael_soares_1996@hotmail.com>
2018-09-07 15:13:32 -0300

    Assignment 1 done.

--- Imagens/Assignment 1/assignment_1.py
+++ Imagens/Assignment 1/assignment_1.py
@@ -1,24 +1,28 @@
 import argparse
 import cv2
 from calcula_dist import calcula_dist
-from transform import trans_gray
+from transform import trans_gray_eq
 from olha_vizinhos import olha_vizinhos
 import numpy as np
 
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument("-i1", "--image1", required=True, help="Caminho para a imagem 1 e 2")
+    ap.add_argument("-i1", "--image1", required = True, help="Path para a imagem Mars.bmp")
+    ap.add_argument("-i2", "--image2", required = True, help = "Path para a imagem spots.tif")
     args = vars(ap.parse_args())
 
-    image = cv2.imread(args["image1"])
+    image_1 = cv2.imread(args["image1"]) # Abre a imagem Mars.bmp
+    image_2 = cv2.imread(args["image2"]) # Abre a imagem spots.tif
 
     # cv2.rectangle(image, (415, 260), (430, 290), 0, -1) # Desenha um retângulo na base
-    # acha_caminho(image)
-    conta_componentes(image)
+    acha_caminho(image_1)
+    conta_componentes(image_2, "preto")
+    image_neg = cv2.bitwise_not(image_2) # Usamos a imagem "negada" para encontrar os buracos :)
+    conta_componentes(image_neg, "buracos")
     cv2.destroyAllWindows()
 
 def acha_caminho(image):
-    img = trans_gray(image)
+    img = trans_gray_eq(image) # Imagem cinza equalizada passa a ter um único canal a partir daqui!
     p_origem = [415, 260]
     p_atual = [415, 260]
     p_destino = [1000, 815]
@@ -51,40 +55,53 @@ def acha_caminho(image):
     cv2.namedWindow("Saida", cv2.WINDOW_NORMAL) 
     cv2.imshow("Saida", image)
     cv2.imwrite("Mars_Caminho.bmp", image)
+    cv2.imwrite("Mars_Eq.bmp", img)
     cv2.waitKey(0)
 
     print("Imagens salvas!")
     print("O caminho de (%d;%d) até (%d;%d) foi feito em %d pixels com %d unidades de energia." 
             %(p_origem[0], p_origem[1], p_destino[0], p_destino[1], num_pixels, energy))
 
-def conta_componentes(img):
-    count = 1
+def conta_componentes(img, tarefa): # A estratégia BFS será usada para contar os componentes e rotulá-los
+    count = 1 # Contador do número de componentes
     fila = [] # Fila First in First Out para fazer busca em largura
 
     width = img.shape[1] 
     height = img.shape[0] 
     arr = np.zeros((width + 2, height + 2, 3), dtype = int) + 255
+    """ Criamos um numpy array, que é como as imagens são representadas em OpenCV para python, 
+        cheios de 0 para tratar os vizinhos de pixels da borda da imagem. 
+        Depois colocamos a imagem original no centro deste array, através do próximo comando.
+    """ 
+    arr[1:(width+1), 1:(height+1)] = img[0:width, 0:height] 
 
-    arr[1:(width+1), 1:(height+1)] = img[0:width, 0:height]
-
-    for j in range(height):
-        for i in range(width):
-            if(arr[j, i, 0] == 0):
-                fila.append([j, i])
-                arr[j, i, 0] = count
-                while (fila):
-                    pixel = fila.pop(0)
-                    lista_vizinhos = olha_vizinhos(pixel[0], pixel[1])
-                    for vizinho in lista_vizinhos:
+    # Começamos a percorrer o numpy array a partir do 1, pois o 0 é apenas o contorno.
+    for j in range(1, height): # linhas: altura na imagem são as linhas (eixo y)
+        for i in range(1, width): # colunas: largura na imagem são as colunas (eixo x)
+            if(arr[j, i, 0] == 0): # Verificamos se o pixel é preto olhando o primeiro canal
+                fila.append([j, i]) # Se for preto colocamos na fila First in First Out.
+                arr[j, i, 0] = count # Marcamos o pixel atual como visitado, setando-o para count
+                while (fila): # Enquanto a fila for diferente de vazio
+                    pixel = fila.pop(0) # Retiramos o primeiro elemento (First in First Out).
+                    lista_vizinhos = olha_vizinhos(pixel[0], pixel[1]) # Olhamos todos os vizinhos
+                    for vizinho in lista_vizinhos: # Analisamos os vizinhos
                         y = vizinho[1]
                         x = vizinho[0]
-                        if(arr[y, x, 0] == 0):
-                            arr[y, x, 0] = count
-                            fila.append([y, x])
-                count += 1
+                        if(arr[y, x, 0] == 0): # Se for preto, ou seja, não tiver marcado ainda
+                            arr[y, x, 0] = count # Marcamos para que não seja analisado novamente
+                            fila.append([y, x]) # E colocamos na fila
+                count += 1 # Incrementamos count
 
-    print("Eu encontrei %d componentes conectados" %(count-1))
-
+    if(tarefa == "preto"):
+        print("%d componentes conectados foram encontrados em spots.tif." %(count-1)) 
+        # count-1, pois count já começa com valor 1
+    elif(tarefa == "buracos"):
+        print("%d buracos foram encontrados em spots.tif." %(count-2)) 
+        """ count-2, pois:
+        1 - A variável count já começa com valor 1;
+        2 - O fundo branco vai ser contado como um componente conectado;
+        OBS: Na antiga imagem spots.tif precisava fazer count-3, pois havia um pixel branco isolado.
+        """
 
 if __name__ == "__main__":
     main()
--- Imagens/Assignment 1/transform.py
+++ Imagens/Assignment 1/transform.py
@@ -13,7 +13,7 @@ cálculos com floats, precisamos desta função.
 A função cv2.split() é usada para separar os 3 canais da imagem (Blue, Green e Red).
 """
 
-def trans_gray(image):
+def trans_gray_eq(image):
     (B, G, R) = cv2.split(image)
 
     Y = 0.299*R + 0.587*G + 0.114*B
@@ -28,6 +28,61 @@ def trans_gray(image):
     cv2.waitKey(0)
     
     # trans = cv2.merge([Y, Cr, Cb])
-    cv2.imshow("Grey", Y)
+    return equalizar(Y) # Retornamos a imagem cinza equalizada 
+
+def equalizar(img):
+    width = img.shape[1] 
+    height = img.shape[0] 
+    menor_intensidade = np.amin(img) # Pego o valor do pixel mais escuro da imagem
+    maior_intensidade = np.amax(img) # Pego o valor do pixel mais claro  da imagem
+    
+    probabilidades = np.zeros(maior_intensidade + 1, dtype = int) 
+    """ probabilidades foi criado que o algoritmo rode mais rápido. Ele funciona da seguinte forma:
+        - É um numpy array onde todos os elementos iniciam com zero.
+        - Cada elemento é a quantidade de vezes que o pixel com aquela intensidade aparece na imagem.
+            Por exemplo, se probabilidades[0] = 1, significa que o pixel com intensidade 0 aparece 
+            uma vez na imagem.
+        - O numpy array possui maior_intensidade + 1 elementos, devido a forma que ele funciona.
+        - Note que os elementos dos índices x, tal que x < menor_intensidade terão o valor sempre zero. 
+          Entretanto, isso não importa.
+    """
+
+   
+    for j in range(height): # Percorro a imagem para saber a quant. de pixels em cada tom, o h(r_k)= n_k
+        for i in range(width):
+            if (probabilidades[img[j, i]] > 0): 
+                probabilidades[img[j, i]] += 1
+            else:
+                probabilidades[img[j, i]] = 1
+    """ No primeiro if do for acima verificamos se o pixel x já apareceu antes.
+        Se ele já apareceu, incrementamos a quantidade de vezes que ele apareceu.
+        Fazemos isso acessando o conteúdo do elemento x do array, que guarda a quantidade de vezes
+        que x aparece na imagem.
+    """
+
+    probabilidades = (probabilidades)/(height*width)
+     # Calculo a probabilidade de cada pixel na imagem n_k/(quant. pixels)
+    
+    for i in range(1, maior_intensidade + 1): # Faço soma de prefixos para obter prob. acumulada
+        probabilidades[i] += probabilidades[i-1]
+    """ Nesse for da da linha 37 é feito soma de prefixos. A soma de prefixos funciona da seguinte forma:
+        - O elemento 0 fica com o seu valor original.
+        - A partir do elemento 1, somamos o valor do elemento mais o elemento anterior.
+        - Assim, elemento 1 = elemento 1 + 0, elemento 2 = elemento 2 + elemento 1 e assim por diante.
+    """
+
+    probabilidades = probabilidades*(maior_intensidade-1)
+    # Multiplico o valor antigo por (L-1) para obter s_k = T(r_k). Aqui já tenho os somatórios guardados.
+
+    probabilidades = np.around(probabilidades)
+    # Arrendondo o valor e obtenho s a ser usado na imagem.
+    # Se x = 4.5, então np.around(x) = 4 que é o par mais próximo.
+
+    for j in range(height): # Percorro a imagem para pintá-la
+        for i in range(width):
+            img[j, i] = probabilidades[img[j, i]] # Substituo o pixel pelo valor de s calculado acima
+
+    cv2.imshow("Imagem Equalizada", img)
     cv2.waitKey(0)
-    return Y
+    cv2.destroyAllWindows()
+    return img
