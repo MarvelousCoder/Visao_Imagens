08083bd23c58989f44a02e4a30a53b97655214d2
MarvelousCoder <MarvelousCoder@github.com>
2018-09-22 19:34:26 -0300

    Atividade 1 finalizada.

--- Computação Gráfica/Exercícios/Atividade1.cpp
+++ Computação Gráfica/Exercícios/Atividade1.cpp
@@ -2,200 +2,14 @@
 #include <cmath>
 #include <iostream>
 
-GLsizei winWidth = 600, winHeight = 600;
-
-/* Set range for world coordinates. */
-GLfloat xwcMin = 0.0, xwcMax = 225.0;
-GLfloat ywcMin = 0.0, ywcMax = 225.0;
-
-typedef GLfloat Matrix3x3 [3][3];
-Matrix3x3 matComposite;
+void rotacao2D(GLfloat angle);
+void translacao2D(GLfloat tx, GLfloat ty);
+void escala2D(GLfloat x, GLfloat y);
 
 char tecla = 'n';
-char arrow = 'n';
-GLdouble theta = 1.0;
-GLfloat g_tx = 0.0, g_ty = 0.0;
-GLfloat g_sx = 1.0, g_sy = 1.0;
-
-
-class wcPt2D {
-    public:
-    GLfloat x, y;
-};
-
-void init (void)
-{
-    /* Set color of display window to white. */
-    glClearColor (1.0, 1.0, 1.0, 0.0);
-}
-
-/* Construct the 3 x 3 identity matrix. */
-void matrix3x3SetIdentity (Matrix3x3 matIdent3x3)
-{
-    GLint row, col;
-    for (row = 0; row < 3; row++)
-        for (col = 0; col < 3; col++)
-            matIdent3x3 [row][col] = (row == col);
-}
-
-/* Premultiply matrix m1 times matrix m2, store result in m2. */
-void matrix3x3PreMultiply (Matrix3x3 m1, Matrix3x3 m2)
-{
-    GLint row, col;
-    Matrix3x3 matTemp;
-    for (row = 0; row < 3; row++)
-        for (col = 0; col < 3 ; col++)
-        matTemp [row][col] = m1 [row][0] * m2 [0][col] + m1 [row][1] *
-                                m2 [1][col] + m1 [row][2] * m2 [2][col];
-    for (row = 0; row < 3; row++)
-        for (col = 0; col < 3; col++)
-            m2 [row][col] = matTemp [row][col];
-}
-
-void translate2D (GLfloat tx, GLfloat ty)
-{
-    Matrix3x3 matTransl;
-    /* Initialize translation matrix to identity. */
-    matrix3x3SetIdentity (matTransl);
-    if(arrow == 'u') g_ty = 5.0;
-    else if (arrow == 'd') g_ty = -5.0;
-    else if (arrow = 'l') g_tx = -5.0;
-    else if (arrow = 'r') g_tx = 5.0;
-    matTransl [0][2] = g_tx;
-    matTransl [1][2] = g_ty;
-
-    /* Concatenate matTransl with the composite matrix. */
-    matrix3x3PreMultiply (matTransl, matComposite);
-}
-
-void rotate2D (wcPt2D pivotPt, GLfloat theta)
-{
-    Matrix3x3 matRot;
-    /* Initialize rotation matrix to identity. */
-    matrix3x3SetIdentity (matRot);
-
-    matRot [0][0] = cos (theta);
-    matRot [0][1] = -sin (theta);
-    matRot [0][2] = pivotPt.x * (1 - cos (theta)) + pivotPt.x * sin(theta);
-
-    matRot [1][0] = sin (theta);
-    matRot [1][1] = cos (theta);
-    matRot [1][2] = pivotPt.y * (1 - cos (theta)) - pivotPt.x * sin(theta);
-
-    /* Concatenate matRot with the composite matrix. */
-    if(arrow == 'u') matrix3x3PreMultiply (matRot, matComposite);
-    else if (arrow == 'd') {
-        matRot [0][1] = sin(theta);
-        matRot [0][2] = pivotPt.x * (1 - cos (theta)) - pivotPt.x * sin(theta);
-        matRot [1][0] = -sin (theta);
-        matRot [1][2] = pivotPt.y * (1 - cos (theta)) + pivotPt.x * sin(theta);
-        matrix3x3PreMultiply (matRot, matComposite);
-    }
-}
-
-void scale2D (GLfloat sx, GLfloat sy, wcPt2D fixedPt) 
-{
-    Matrix3x3 matScale;
-
-    matrix3x3SetIdentity(matScale);
-    if(arrow == 'u') g_sx = g_sy = 2.0;
-    else if (arrow = 'd') g_sx = g_sy = 0.5;
-    matScale [0][0] = g_sx;
-    matScale [0][2] = (1 - g_sx) * fixedPt.x;
-    matScale [1][1] = g_sy;
-    matScale [1][2] = (1 - g_sy) * fixedPt.y;
-
-    /* Concatenate matRot with the composite matrix. */
-    matrix3x3PreMultiply (matScale, matComposite);
-}
-
-/* Using the composite matrix, calculate transformed coordinates. */
-void transformVerts2D (GLint nVerts, wcPt2D * verts)
-{
-    GLint k;
-    GLfloat temp;
-    for (k = 0; k < nVerts; k++) {
-        temp = matComposite [0][0] * verts [k].x + matComposite [0][1] *
-                verts [k].y + matComposite [0][2];
-        verts [k].y = matComposite [1][0] * verts [k].x + matComposite [1][1] *
-                        verts [k].y + matComposite [1][2];
-        verts [k].x = temp;
-    }
-}
-
-void triangle (wcPt2D *verts)
-{
-    GLint k;
-
-    glBegin (GL_TRIANGLES);
-        for (k = 0; k < 3; k++)
-            glVertex2f (verts [k].x, verts [k].y);
-    glEnd ( ); 
-}
-
-void criaCena(void) 
-{
-    GLint nVerts = 3;
-    wcPt2D verts [3] = { {30.0, 5.0}, {130.0, 5.0}, {80.0, 80.0} }; // Posição inicial do triângulo
-    wcPt2D centroidPt;
-    wcPt2D pivPt, fixedPt; // Parâmetros da transf. geométrica
-
-    GLint k, xSum = 0, ySum = 0;
-    for (k = 0; k < nVerts; k++) {
-        xSum += verts [k].x;
-        ySum += verts [k].y;
-    }
-    centroidPt.x = GLfloat (xSum) / GLfloat (nVerts);
-    centroidPt.y = GLfloat (ySum) / GLfloat (nVerts);
-
-    /* Set geometric transformation parameters. */
-
-    pivPt = centroidPt;
-    fixedPt = centroidPt;
-
-    glClear (GL_COLOR_BUFFER_BIT); // Clear display window.
-
-    // glColor3f (0.0, 0.0, 1.0);
-    // triangle (verts); // Display blue triangle
-
-    /* Initialize composite matrix to identity. */
-    matrix3x3SetIdentity (matComposite);
-
-    /* Construct composite matrix for transformation sequence. */
-    
-    switch (tecla)
-    {
-        case 'r':
-            rotate2D (pivPt, theta);    
-            break;
-        case 's':
-            scale2D (g_sx, g_sy, fixedPt);
-            break;
-        case 't':
-            translate2D (g_tx, g_ty);
-            break;
-        default:
-            break;
-    }
-
-    /* Apply composite matrix to triangle vertices. */
-    transformVerts2D (nVerts, verts);
-
-    glColor3f (0.0, 0.0, 1.0); // Set color for transformed triangle.
-    triangle (verts); // Display red triangle.
-
-    glFlush ( );
-    glutPostRedisplay();
-}
-
-void winReshape (GLint newWidth, GLint newHeight)
-{
-    glMatrixMode (GL_PROJECTION);
-    glLoadIdentity ( );
-    gluOrtho2D (xwcMin, xwcMax, ywcMin, ywcMax);
-
-    glClear (GL_COLOR_BUFFER_BIT);
-}
+int cnt = 1;
+GLfloat cx = (0.5f + 0.5f + 0.7f + 0.7f)/4.0;
+GLfloat cy = (0.5f + 0.7f + 0.7f + 0.5f)/4.0;
 
 void operacao(GLubyte key, GLint xMouse, GLint yMouse) 
 {
@@ -220,43 +34,127 @@ void executaOperacao(GLint specialKey, GLint xMouse, GLint yMouse)
     switch (specialKey)
     {
         case GLUT_KEY_UP:
-            arrow = 'u';
+            if(tecla == 'r') { rotacao2D(0.9f); cnt++; }
+            else if (tecla == 's') escala2D(2.0f, 2.0f);
+            else if (tecla == 't') translacao2D(0.0f, 0.1f);
             break;
         case GLUT_KEY_DOWN:
-            arrow = 'd';
+            if(tecla == 'r') { rotacao2D(-0.9f); cnt--; }
+            else if (tecla == 's') escala2D(0.5f, 0.5f);
+            else if (tecla == 't') translacao2D(0.0f, -0.1f);
             break;
         case GLUT_KEY_LEFT:
-            arrow = 'l';
+            if (tecla == 't') translacao2D(-0.1f, 0.0f);
             break;
         case GLUT_KEY_RIGHT:
-            arrow = 'r';
+            if (tecla == 't') translacao2D(0.1f, 0.0f);
             break;
         default:
-            arrow = 'n';
             break;
     }
-    /* redesenha a tela */
-    // glutDisplayFunc(criaCena);
-    // glutPostRedisplay();
 }
+
+void rotacao2D(GLfloat angle) 
+{
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glPopMatrix();
+    glTranslatef(cx, cy, 0.0f);
+
+    glRotatef(angle, 0.0f, 0.0f, 1.0f);
+    glTranslatef(-cx, -cy, 0.0f);
+    glPushMatrix();
+}
+
+void translacao2D(GLfloat tx, GLfloat ty) 
+{
+    /* Habilita a matriz de transformações geométricas */
+    glMatrixMode(GL_MODELVIEW);
+
+    /* Reseta a matriz de modelo para transformações geométricas, colocando-a como
+    Identidade */
+    glLoadIdentity();
+
+    glPopMatrix();
+    /* Vamos rotacionar o objeto para a origem */
+    glTranslatef(cx, cy, 0.0f);
+    glRotatef(-cnt*0.9f, 0.0f, 0.0f, 1.0f);
+    glTranslatef(-cx, -cy, 0.0f);
+    glPushMatrix(); 
+
+    glLoadIdentity();
+    glPopMatrix(); /* Pegamos a matriz do objeto na origem */
+    glTranslatef(tx, ty, 0.0f); /* Fazemos a translação necessária */
+
+    /* Rotacionamos o objeto de volta para onde estava */
+    glTranslatef(cx, cy, 0.0f);
+    glRotatef(cnt*0.9f, 0.0f, 0.0f, 1.0f);
+    glTranslatef(-cx, -cy, 0.0f);
+
+    glPushMatrix(); /* Salva o estado da matriz na pilha */
+
+}
+
+void escala2D(GLfloat x, GLfloat y) 
+{
+    glMatrixMode(GL_MODELVIEW);
+
+    glLoadIdentity();
+    glPopMatrix();
+    glTranslatef(cx, cy, 0.0f);
+    glScalef(x, y, 0.0f);
+    glTranslatef(-cx, -cy, 0.0f);
+
+    glPushMatrix();
+}
+
+void drawRectangle() 
+{
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glBegin(GL_QUADS);
+        glVertex2f(0.5f, 0.5f);
+        glVertex2f(0.5f, 0.7f);
+        glVertex2f(0.7f, 0.7f);
+        glVertex2f(0.7f, 0.5f);
+    glEnd();
+}
+
+void criaCena() 
+{
+    /* Limpa o buffer e coloca a cor padrão de fundo */
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    drawRectangle();
+    glFlush();
+    glutPostRedisplay();
+}
+
 
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
+    /* Iniciar a GLUT para que recursos computacionais sejam disponibilizados para este programa */
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 
+    glutInitWindowSize(600, 600);
+
+    /* Coordenadas da tela inicial da janela do OpenGL */
     glutInitWindowPosition(50, 50);
-    glutInitWindowSize(winWidth, winHeight);
-    glutCreateWindow("Atividade pratica 1");
 
-    init ( );
-    glutDisplayFunc (criaCena);
-    glutReshapeFunc (winReshape);
+    /* Cria a janela */
+    glutCreateWindow("Atividade1");
 
-    
+    /* Função que cria processo de renderização */
+    // Objeto quadrado;
+    glutDisplayFunc(criaCena);
+
+    /* Habilitar o teclado usando recursos da GLUT, chama o método keyboard_special */
     glutKeyboardFunc(operacao);
     glutSpecialFunc(executaOperacao);
 
+
+    /* Coloca esse programa OpenGL em loop infinito */
     glutMainLoop();
     return 0;
 }
